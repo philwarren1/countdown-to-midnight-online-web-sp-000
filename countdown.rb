652287d4cba241714852ba8adf6bc560f4fe66fc
Philip Warren <philwarren1@gmail.com>
2020-07-23 08:06:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,14 @@
 #write your code here
 
-def countdown
+def countdown(10)
+  x = 10
+  while x > 0
+   puts "#{x} SECOND(S)!"
+   x -= 1
+  end
 end
-
+x = 1
+while x < 10
+  puts "#{x} is less than 10"
+  x += 1
+end
